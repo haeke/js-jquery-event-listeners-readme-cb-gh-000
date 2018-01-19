02c6e65e1d4abe42793c1ffba159a00ec43840fe
edwin rivera <waehake@gmail.com>
2018-01-19 15:54:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,11 @@
 $(document).ready(function(){
 
 // call functions here
+  function getIt() {
+    $('p').on('click', function() {
+      alert('hey');   
+    });
+  }
   $('form').on('submit', function() {
     if ($('input:first').val() === 'correct') {
       alert('your form iwas going to be submitted now');
