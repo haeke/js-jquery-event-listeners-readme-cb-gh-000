fdb5d2858b4177847233c6155bc56f04a1f47aa1
edwin rivera <waehake@gmail.com>
2018-01-19 15:52:48 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,12 @@
 $(document).ready(function(){
 
 // call functions here
-
+  $('form').on('submit', function() {
+    if ($('input:first').val() === 'correct') {
+      alert('your form iwas going to be submitted now');
+      return ;
+    }
+    alert('you entered the wrong value');
+    return;
+  });
 });
